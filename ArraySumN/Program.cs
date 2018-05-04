73ad5b871ec7406cb507b004fb494d10d53dfaf9
lutfiqaraman <lutfiqaraman@gmail.com>
2018-05-04 22:35:32 +0300

    Done

--- ArraySumN/Program.cs
+++ ArraySumN/Program.cs
@@ -10,6 +10,17 @@ namespace ArraySumN
     {
         static void Main(string[] args)
         {
+            int[] a1 = { 1, 3, 4, 3, -2 };
+            Random rnd = new Random();
+            int n = rnd.Next(1, 100);
+
+            for (int i = 0; i < a1.Length; i++)
+            {
+                a1[i] += n - a1[i];
+                Console.Write(a1[i] + "\t");
+            }
+
+            Console.ReadKey();
         }
     }
 }
